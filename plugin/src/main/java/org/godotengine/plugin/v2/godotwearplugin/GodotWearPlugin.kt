fa87b82a86e7884b07b5dd59d4c0c971c43f5d5a
Mike <elleo@gnu.org>
2026-08-01 20:37:45 +0100

    Begin adding AOD support

--- plugin/src/main/java/org/godotengine/plugin/v2/godotwearplugin/GodotWearPlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/v2/godotwearplugin/GodotWearPlugin.kt
@@ -4,32 +4,54 @@ import android.util.Log
 import android.widget.Toast
 import android.content.Context
 import android.content.pm.PackageManager
+import android.app.Activity
 import androidx.wear.ambient.AmbientLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
 
 class GodotWearPlugin(godot: Godot): GodotPlugin(godot) {
 
+    private var ambientActive: Boolean = false
+
+    private val ambientCallback = object : AmbientLifecycleObserver.AmbientLifecycleCallback {
+
+        override fun onEnterAmbient(ambientDetails: AmbientLifecycleObserver.AmbientDetails) {
+            ambientActive = true
+        }
+
+        override fun onUpdateAmbient() {
+        }
+
+        override fun onExitAmbient() {
+            ambientActive = false
+        }
+    }
+
+    private val ambientObserver: AmbientLifecycleObserver by lazy {
+        AmbientLifecycleObserver(godot.getActivity() as Activity, ambientCallback)
+    }
+
     override fun getPluginName() = BuildConfig.GODOT_PLUGIN_NAME
 
     @UsedByGodot
     fun isWear(): Boolean {
-        return getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_WATCH);
+        return getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_WATCH)
     }
 
     @UsedByGodot
     fun enableAOD() {
-
+        (getContext() as LifecycleOwner).lifecycle.addObserver(ambientObserver)
     }
 
     @UsedByGodot
     fun disableAOD() {
-
+        (getContext() as LifecycleOwner).lifecycle.removeObserver(ambientObserver)
     }
 
     @UsedByGodot
     fun AODActive(): Boolean {
-        return false;
+        return ambientActive;
     }
 }
